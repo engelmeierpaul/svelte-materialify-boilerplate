32cefe141f27241fcd4e3baba14c8dc45722473d
Paul Engelmeier <allwqre@pm.me>
2021-02-26 04:47:21 +0100

    Materialify Darkmode Demo

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,18 @@
 <script lang="ts">
     import {MaterialApp} from 'svelte-materialify';
+    import Switch from 'svelte-materialify/src/components/Switch';
 
-    let theme: 'light' | 'dark' = 'dark';
+    let darkmode = true;
+    $: theme = darkmode ? 'dark' : 'light';
 </script>
 
-<MaterialApp theme="{theme}">
+<MaterialApp {theme}>
   <main>
     <h1>Svelte+Materialify!</h1>
+    <Switch class="justify-center" color="red" bind:checked={darkmode} inset>Dark Mode</Switch>
     <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-    <p>Visit the <a href="https://svelte-materialify.vercel.app/">Materialify Website</a> to learn about Materialify and its components.</p>
+    <p>Visit the <a href="https://svelte-materialify.vercel.app/">Materialify Website</a> to learn about Materialify and
+      its components.</p>
   </main>
 </MaterialApp>
 
@@ -18,6 +22,10 @@
     padding: 1em;
     max-width: 240px;
     margin: 0 auto;
+  }
+
+  div {
+    background-color: #00acc1 !important;
   }
 
   h1 {
